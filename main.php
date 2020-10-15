f501582eb859e7373d6786fd42c4557595d74acf
julian443 <63303982+julian443@users.noreply.github.com>
2020-10-15 23:25:49 +0200

    friend page with use cases

---
+++ main.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Friendbook</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand bg-light navigation-clean">
+        <div class="container"><a class="navbar-brand" href="index.php">
+                <img src="assets\img\friendbook_logo.png" alt="Friendbook Logo">
+            </a>
+            <div class="collapse navbar-collapse" id="navcol-1">
+
+            <!-- Search Friend -->
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search a friend" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+
+                <!-- Nav Add Friend -->
+                <a class="btn btn-success ml-auto mr-0" role="button" href="#" data-toggle="modal" data-target="#registerModal">Freund hinzufügen</a>
+            </div>
+        </div>
+    </nav>
+    <header class="masthead text-white" style="background-color: #baddff;">
+        <div class="container">
+            <h1>Freunde</h1>
+
+            <div class="row mt-5 mb-5">
+                <div class="col-lg-4">
+
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <!-- Profile Pic -->
+                        <div class="text-center">
+                            <!-- <img src="assets\img\friendbook_logo.png" alt="Friendbook Logo"> -->
+                            <h1>[PROFILE PIC]</h1>
+                            <h3>Julian Mathis</h3>
+                        </div>
+                        <h5>Land: Schweiz</h5>
+                        <h5>Canton: Basel</h5>
+                        <h5>Gender: Chad</h5>
+                        <h5>Adresse: Adresse [+housenum], PLZ, Ort</h5>
+                        <h5>Email: [Email]</h5>
+                        <h5>Handy: 076 528 21 82</h5>
+                        <a class="btn btn-danger ml-auto mr-0" role="button" href="#">Freund löschen</a>
+
+                    </div>
+
+                </div>
+
+                <div class="col-lg-4">
+
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <!-- Profile Pic -->
+                        <div>
+                            <!-- <img src="assets\img\friendbook_logo.png" alt="Friendbook Logo"> -->
+                            <h1>[PROFILE PIC]</h1>
+                        </div>
+                        <h3>Julian Mathis</h3>
+                        <h5>Land: Schweiz</h5>
+                        <h5>Canton: Basel</h5>
+                        <h5>Gender: Chad</h5>
+                        <h5>Adresse: Adresse [+housenum], PLZ, Ort</h5>
+                        <h5>Email: [Email]</h5>
+                        <h5>Handy: 076 528 21 82</h5>
+                        <a class="btn btn-danger ml-auto mr-0" role="button" href="#">Freund löschen</a>
+
+                    </div>
+
+                </div>
+
+                <div class="col-lg-4">
+
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <!-- Profile Pic -->
+                        <div>
+                            <!-- <img src="assets\img\friendbook_logo.png" alt="Friendbook Logo"> -->
+                            <h1>[PROFILE PIC]</h1>
+                        </div>
+                        <h3>Julian Mathis</h3>
+                        <h5>Land: Schweiz</h5>
+                        <h5>Canton: Basel</h5>
+                        <h5>Gender: Chad</h5>
+                        <h5>Adresse: Adresse [+housenum], PLZ, Ort</h5>
+                        <h5>Email: [Email]</h5>
+                        <h5>Handy: 076 528 21 82</h5>
+                        <a class="btn btn-danger ml-auto mr-0" role="button" href="#">Freund löschen</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div class="row mt-5 mb-5">
+                <div class="col-lg-4">
+
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <!-- Profile Pic -->
+                        <div>
+                            <!-- <img src="assets\img\friendbook_logo.png" alt="Friendbook Logo"> -->
+                            <h1>[PROFILE PIC]</h1>
+                        </div>
+                        <h3>Julian Mathis</h3>
+                        <h5>Land: Schweiz</h5>
+                        <h5>Canton: Basel</h5>
+                        <h5>Gender: Chad</h5>
+                        <h5>Adresse: Adresse [+housenum], PLZ, Ort</h5>
+                        <h5>Email: [Email]</h5>
+                        <h5>Handy: 076 528 21 82</h5>
+                        <a class="btn btn-danger ml-auto mr-0" role="button" href="#">Freund löschen</a>
+
+                    </div>
+
+                </div>
+
+                <div class="col-lg-4">
+
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <!-- Profile Pic -->
+                        <div>
+                            <!-- <img src="assets\img\friendbook_logo.png" alt="Friendbook Logo"> -->
+                            <h1>[PROFILE PIC]</h1>
+                        </div>
+                        <h3>Julian Mathis</h3>
+                        <h5>Land: Schweiz</h5>
+                        <h5>Canton: Basel</h5>
+                        <h5>Gender: Chad</h5>
+                        <h5>Adresse: Adresse [+housenum], PLZ, Ort</h5>
+                        <h5>Email: [Email]</h5>
+                        <h5>Handy: 076 528 21 82</h5>
+                        <a class="btn btn-danger ml-auto mr-0" role="button" href="#">Freund löschen</a>
+
+                    </div>
+
+                </div>
+
+                <div class="col-lg-4">
+
+                    <div class="mx-auto features-icons-item mb-5 mb-lg-0 mb-lg-3">
+                        <!-- Profile Pic -->
+                        <div>
+                            <!-- <img src="assets\img\friendbook_logo.png" alt="Friendbook Logo"> -->
+                            <h1>[PROFILE PIC]</h1>
+                        </div>
+                        <h3>Julian Mathis</h3>
+                        <h5>Land: Schweiz</h5>
+                        <h5>Canton: Basel</h5>
+                        <h5>Gender: Chad</h5>
+                        <h5>Adresse: Adresse [+housenum], PLZ, Ort</h5>
+                        <h5>Email: [Email]</h5>
+                        <h5>Handy: 076 528 21 82</h5>
+                        <a class="btn btn-danger ml-auto mr-0" role="button" href="#">Freund löschen</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+    </header>
+    <footer class="footer bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 my-auto h-100 text-center text-lg-left">
+                    <ul class="list-inline mb-2">
+                        <li class="list-inline-item"><a href="#">About</a></li>
+                        <li class="list-inline-item"><span>⋅</span></li>
+                        <li class="list-inline-item"><a href="#">Contact</a></li>
+                        <li class="list-inline-item"><span>⋅</span></li>
+                        <li class="list-inline-item"><a href="#">Terms of &nbsp;Use</a></li>
+                        <li class="list-inline-item"><span>⋅</span></li>
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    </ul>
+                    <p class="text-muted small mb-4 mb-lg-0">© Friendbook 2020. All Rights Reserved.</p>
+                </div>
+                <div class="col-lg-6 my-auto h-100 text-center text-lg-right">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-facebook fa-2x fa-fw"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-twitter fa-2x fa-fw"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-instagram fa-2x fa-fw"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+</body>
+
+</html>
