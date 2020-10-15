413ced9b747f0a9d9df75fa81ad1b574580a6ba1
julian443 <63303982+julian443@users.noreply.github.com>
2020-10-15 17:19:54 +0200

    register + login

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ include('dbconnector.inc.php');
 
 // Initialisierung
 $error = $message =  '';
-$firstname = $lastname = $email = $username = '';
+$firstname = $lastname = $email = '';
 
 // Wurden Daten mit "POST" gesendet?
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($firstname)) {
   // Ausgabe des gesamten $_POST Arrays
   echo "<pre>";
   print_r($_POST);
@@ -53,19 +53,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $error .= "Geben Sie bitte eine Emailadresse ein.<br />";
   }
 
-  // username ausgefüllt?
-  if (isset($_POST['username'])) {
-    //trim and sanitize
-    $username = trim($_POST['username']);
-
-    //mindestens 1 Zeichen , entsprich RegEX
-    if (empty($username) || !preg_match("/(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]{6,30}/", $username)) {
-      $error .= "Geben Sie bitte einen korrekten Usernamen ein.<br />";
-    }
-  } else {
-    $error .= "Geben Sie bitte einen Username ein.<br />";
-  }
-
   // passwort ausgefüllt
   if (isset($_POST['password'])) {
     //trim and sanitize
@@ -81,22 +68,26 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
   // wenn kein Fehler vorhanden ist, schreiben der Daten in die Datenbank
   if (empty($error)) {
-    // INPUT Query erstellen, welches firstname, lastname, username, password, email in die Datenbank schreibt
-    $insertStatement = "INSERT into users(firstname, lastname, username, password, email) VALUES (?, ?, ?, ?, ?)";
+    // INPUT Query erstellen, welches firstname, lastname, password, email in die Datenbank schreibt
+    $insertStatement = "INSERT into users(fname, name, pword, email) VALUES (?, ?, ?, ?)";
     // Query vorbereiten mit prepare();
     $stmt = $mysqli->prepare($insertStatement);
     // Parameter an Query binden mit bind_param();
     $password = password_hash($password, PASSWORD_DEFAULT);
-    $stmt->bind_param("sssss", $firstname, $lastname, $username, $password, $email);
+    $stmt->bind_param("ssss", $firstname, $lastname, $password, $email);
     // query ausführen mit execute();
     $stmt->execute();
     // Verbindung schliessen
     $stmt->close();
     // Weiterleitung auf login.php
-    header("Location: login.php");
+    header("Location: main.php");
   }
 }
 
+// Login POST verify
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($firstname) == false) {
+
+}
 
 //
 ?>
@@ -117,9 +108,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 <body>
   <nav class="navbar navbar-light navbar-expand bg-light navigation-clean">
-    <div class="container"><a class="navbar-brand" href="#" style="font-size: 40px;color: rgba(30,202,239,0.9);font-family: Lato, sans-serif;height: 70px;border-width: 5px;border-style: none;box-shadow: 7px 7px 0px 0px rgba(53,156,179,0.9);width: 208px;padding: 0px;margin: 15px;">Friendbook</a>
-      <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"></button>
-      <div class="collapse navbar-collapse" id="navcol-1"><a class="btn btn-primary ml-auto" role="button" href="#">Einloggen</a></div>
+    <div class="container"><a class="navbar-brand" href="index.php">
+        <img src="assets\img\friendbook_logo.png" alt="Friendbook Logo">
+      </a>
+      <div class="collapse navbar-collapse" id="navcol-1">
+        <!-- Nav Register Button -->
+        <a class="btn btn-success ml-auto mr-0" role="button" href="#" data-toggle="modal" data-target="#registerModal">Registrieren</a>
+        <!-- Nav Login Button -->
+        <a class="btn btn-primary ml-3 mr-0" role="button" href="#" data-toggle="modal" data-target="#loginModal">Einloggen</a>
+      </div>
     </div>
   </nav>
   <header class="masthead text-white text-center" style="background:url('assets/img/bg-masthead.jpg')no-repeat center center;background-size:cover;">
@@ -131,104 +128,104 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
 
-            <!-- Hero Register Button -->
-            <div class="form-row justify-content-center align-items-center">
-              <div class="col-12 col-md-3">
-                <button type="button" class="btn btn-primary btn-block btn-lg" style="text-align: center;" data-toggle="modal" data-target="#registerModal">Anmelden
-                </button>
-              </div>
-
-              <!-- Hero Login Button -->
-              <div class="col-12 col-md-3" style="text-align: center;">
-                <button type="button" class="btn btn-primary btn-block btn-lg" style="text-align: center;" data-toggle="modal" data-target="#loginModal">Einloggen
-                </button>
-              </div>
-
-              <!-- Register Modal -->
-              <div class="modal fade" id=registerModal tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="registerModalLabel" style="color:#2e2e2e">Registrieren</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-
-                    <div class="modal-body">
-                      <!-- Registrierung füer Friendbook Modal -->
-                      <form action="" method="post">
-                        <!-- Vorname -->
-                        <div class="form-group">
-                        <label for="firstname">Vorname *</label>
-                          <input type="text" name="firstname" class="form-control" id="firstname" value="<?php echo $firstname ?>" placeholder="Vorname" maxlength="30" required="true">
-                        </div>
-                        <!-- Nachname -->
-                        <div class="form-group">
-                        <label for="lastname">Nachname *</label>
-                          <input type="text" name="lastname" class="form-control" id="lastname" value="<?php echo $lastname ?>" placeholder="Nachnamen" maxlength="30" required="true">
-                        </div>
-                        <!-- Email -->
-                        <div class="form-group">
-                        <label for="lastname">Email *</label>
-                          <input type="email" name="email" class="form-control" id="email" value="<?php echo $email ?>" placeholder="Email" maxlength="80" required="true">
-                        </div>
-                        <!-- Password -->
-                        <div class="form-group">
-                        <label for="lastname">Passwort *</label>
-                          <input type="password" name="password" class="form-control" id="password" placeholder="Passwort" pattern="(?=^.{8,}$)((?=.*\d+)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" title="mindestens einen Gross-, einen Kleinbuchstaben, eine Zahl und ein Sonderzeichen, mindestens 8 Zeichen lang,keine Umlaute." maxlength="255" required="true">
-                          <p style="color:darkgray">
-                            *Gross- und Kleinbuchstaben, Zahlen, Sonderzeichen, min. 8 Zeichen, keine Umlaute
-                          </p>
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Schliessen</button>
-                          <button type="button" class="btn btn-primary" type="submit">Registrieren</button>
-                        </div>
-                      </form>
-
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-
-              <!-- Login Modal -->
-              <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="loginModalLabel" style="color:#2e2e2e">Anmelden</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-
-                      <!-- Login -->
-                      <form action="" method="post">
-                        <!-- benutzername -->
-                        <div class="form-group">
-                          <input type="text" name="username" class="form-control" id="username" value="<?php echo $username ?>" placeholder="Benutzername" title="Benutzername" maxlength="30" required="true">
-                        </div>
-                        <!-- password -->
-                        <div class="form-group">
-                          <input type="password" name="password" class="form-control" id="password" placeholder="Passwort" title="Passwort" maxlength="255" required="true">
-                        </div>
-                      </form>
-
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-primary" type="submit">Anmelden</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="clearfix"></div>
+          <!-- Hero Register Button -->
+          <div class="form-row justify-content-center align-items-center">
+            <div class="col-12 col-md-3">
+              <button type="button" class="btn btn-primary btn-block btn-lg" style="text-align: center;" data-toggle="modal" data-target="#registerModal">Registrieren
+              </button>
             </div>
+
+            <!-- Hero Login Button -->
+            <div class="col-12 col-md-3" style="text-align: center;">
+              <button type="button" class="btn btn-primary btn-block btn-lg" style="text-align: center;" data-toggle="modal" data-target="#loginModal">Einloggen
+              </button>
+            </div>
+
+            <!-- Register Modal -->
+            <div class="modal fade" id=registerModal tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="registerModalLabel" style="color:#2e2e2e">Registrieren</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+
+                  <div class="modal-body">
+                    <!-- Registrierung füer Friendbook Modal -->
+                    <form action="" method="post">
+                      <!-- Vorname -->
+                      <div class="form-group">
+                        <label for="firstname">Vorname *</label>
+                        <input type="text" name="firstname" class="form-control" id="firstname" value="<?php echo $firstname ?>" placeholder="Vorname" maxlength="30" required="true">
+                      </div>
+                      <!-- Nachname -->
+                      <div class="form-group">
+                        <label for="lastname">Nachname *</label>
+                        <input type="text" name="lastname" class="form-control" id="lastname" value="<?php echo $lastname ?>" placeholder="Nachnamen" maxlength="30" required="true">
+                      </div>
+                      <!-- Email -->
+                      <div class="form-group">
+                        <label for="lastname">Email *</label>
+                        <input type="email" name="email" class="form-control" id="email" value="<?php echo $email ?>" placeholder="Email" maxlength="80" required="true">
+                      </div>
+                      <!-- Password -->
+                      <div class="form-group">
+                        <label for="lastname">Passwort *</label>
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Passwort" pattern="(?=^.{8,}$)((?=.*\d+)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" title="mindestens einen Gross-, einen Kleinbuchstaben, eine Zahl und ein Sonderzeichen, mindestens 8 Zeichen lang,keine Umlaute." maxlength="255" required="true">
+                        <p style="color:darkgray">
+                          *Gross- und Kleinbuchstaben, Zahlen, Sonderzeichen, min. 8 Zeichen, keine Umlaute
+                        </p>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="reset" class="btn btn-secondary" data-dismiss="modal">Schliessen</button>
+                        <button type="submit" class="btn btn-primary">Registrieren</button>
+                      </div>
+                    </form>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <!-- Login Modal -->
+            <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="loginModalLabel" style="color:#2e2e2e">Anmelden</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+
+                    <!-- Login -->
+                    <form action="" method="post">
+                      <!-- benutzername -->
+                      <div class="form-group">
+                        <input type="text" name="email" class="form-control" id="email" value="<?php echo $email ?>" placeholder="Email" title="Email" maxlength="30" required="true">
+                      </div>
+                      <!-- password -->
+                      <div class="form-group">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Passwort" title="Passwort" maxlength="255" required="true">
+                      </div>
+
+                  </div>
+                  <div class="modal-footer">
+                    <button type="reset" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Anmelden</button>
+                  </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            <div class="clearfix"></div>
+          </div>
         </div>
       </div>
     </div>
