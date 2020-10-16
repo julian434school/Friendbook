8fcb4e4f51364f016625743822d60d3ca5e3525d
johannesvoegtlin <j.g.voegtlin@gmail.com>
2020-10-16 10:12:18 +0200

    Test Commit/Push

--- index.php
+++ index.php
@@ -154,27 +154,37 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($firstname) == false) {
                   </div>
 
                   <div class="modal-body">
-                    <!-- Registrierung füer Friendbook Modal -->
+                    <!-- Registrierung für Friendbook Modal -->
                     <form action="" method="post">
                       <!-- Vorname -->
                       <div class="form-group">
                         <label for="firstname">Vorname *</label>
-                        <input type="text" name="firstname" class="form-control" id="firstname" value="<?php echo $firstname ?>" placeholder="Vorname" maxlength="30" required="true">
+                        <input type="text" name="firstname" class="form-control"
+                        id="firstname" value="<?php echo $firstname ?>" placeholder="Vorname"
+                        maxlength="30" required="true">
                       </div>
                       <!-- Nachname -->
                       <div class="form-group">
                         <label for="lastname">Nachname *</label>
-                        <input type="text" name="lastname" class="form-control" id="lastname" value="<?php echo $lastname ?>" placeholder="Nachnamen" maxlength="30" required="true">
+                        <input type="text" name="lastname" class="form-control"
+                        id="lastname" value="<?php echo $lastname ?>" placeholder="Nachnamen"
+                        maxlength="30" required="true">
                       </div>
                       <!-- Email -->
                       <div class="form-group">
                         <label for="lastname">Email *</label>
-                        <input type="email" name="email" class="form-control" id="email" value="<?php echo $email ?>" placeholder="Email" maxlength="80" required="true">
+                        <input type="email" name="email" class="form-control"
+                        id="email" value="<?php echo $email ?>" placeholder="Email"
+                        maxlength="80" required="true">
                       </div>
                       <!-- Password -->
                       <div class="form-group">
                         <label for="lastname">Passwort *</label>
-                        <input type="password" name="password" class="form-control" id="password" placeholder="Passwort" pattern="(?=^.{8,}$)((?=.*\d+)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" title="mindestens einen Gross-, einen Kleinbuchstaben, eine Zahl und ein Sonderzeichen, mindestens 8 Zeichen lang,keine Umlaute." maxlength="255" required="true">
+                        <input type="password" name="password" class="form-control"
+                        id="password" placeholder="Passwort"
+                        pattern="(?=^.{8,}$)((?=.*\d+)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+                        title="mindestens einen Gross-, einen Kleinbuchstaben, eine Zahl und ein Sonderzeichen, mindestens 8 Zeichen lang,keine Umlaute."
+                        maxlength="255" required="true">
                         <p style="color:darkgray">
                           *Gross- und Kleinbuchstaben, Zahlen, Sonderzeichen, min. 8 Zeichen, keine Umlaute
                         </p>
@@ -207,11 +217,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($firstname) == false) {
                     <form action="" method="post">
                       <!-- benutzername -->
                       <div class="form-group">
-                        <input type="text" name="email" class="form-control" id="email" value="<?php echo $email ?>" placeholder="Email" title="Email" maxlength="30" required="true">
+                        <input type="text" name="email" class="form-control"
+                        id="email" value="<?php echo $email ?>" placeholder="Email"
+                        title="Email" maxlength="30" required="true">
                       </div>
                       <!-- password -->
                       <div class="form-group">
-                        <input type="password" name="password" class="form-control" id="password" placeholder="Passwort" title="Passwort" maxlength="255" required="true">
+                        <input type="password" name="password" class="form-control"
+                        id="password" placeholder="Passwort" title="Passwort"
+                        maxlength="255" required="true">
                       </div>
 
                   </div>
